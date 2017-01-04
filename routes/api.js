d2b0f0980da3105538b3b4626ae1775ae29a3650
Jeff Namenye <jeffnamenye@hotmail.com>
2017-01-04 17:25:35 -0500

    made my routes an add models

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+//this sets up to link my url generator model
+const est = require('../models/url');
+
+//has you use express as a parameter of express
+module.exports = (express) => {
+
+  const router = express.Router();
+
+  //this is my post method
+  router.post('/api', (req, res) =>{
+    //this is to respond with links
+    res.json(est.genURL(req.body.link));
+  });
+  //this is to return the data
+  return router;
+};
